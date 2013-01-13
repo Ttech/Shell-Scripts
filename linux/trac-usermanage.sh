c129b755467391b9b7b5b3c83bc8eb03d95ea859
Joe <ttech@mostlynothing.info>
2013-01-13 12:41:57 -0800

    Fixed a bug in the logic of checking for correct number of arguments

--- linux/trac-usermanage.sh
+++ linux/trac-usermanage.sh
@@ -28,25 +28,25 @@ function print_trac_users(){
 }
 
 function manage_user(){
-	echo htdigest $trac_path/conf/trac.htdigest \"$realm\" $1
-	echo trac-admin $trac_path permission add $1 $2
+	htdigest "$trac_path/conf/trac.htdigest" "$realm" $1
+	trac-admin $trac_path permission add $1 $2
 }
 
 function main(){
 	if [[ $1 == "-h" || -z $1 ]]; then
 		filename=$(basename $0)
-		echo -e "$filename -lih [user] [capability]"
+		echo -e "$filename -lihs [user] [capability]"
 		echo -e "\t\t -h Displays this command"
 		echo -e "\t\t -i Interactive Run"
 		echo -e "\t\t -s Create configuration file"
 		echo -e "\t\t -l List current users!"
-    elif [ $1 == "-i" ]; then
+	elif [ $1 == "-i" ]; then
 		echo "This method has not been implimented yet"
 	elif [ $1 == "-s" ]; then
 		echo "Interactive Configuration Creation"
 		echo "Trac Project Path:"
 		read trac_path
-		echo "Trac Realm"
+		echo "Trac Realm:"
 		read trac_realm
 
 		echo -e "trac_path=\"$trac_path\"\nrealm=\"$trac_realm\"" \
@@ -55,7 +55,11 @@ function main(){
 	elif [ $1 == "-l" ]; then
 		print_trac_users
 	else
-		manage_user $1 $2
+		if [ -n $2 ]; then
+			manage_user $1 $2
+		else
+			echo "Incorrect Arguments"
+		fi
 	fi
 }
 
