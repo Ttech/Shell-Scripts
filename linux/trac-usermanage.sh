b5152a1a4fcfeae1f77ab8eb3459dbcf64d5b20b
Joe <ttech@mostlynothing.info>
2013-01-13 14:57:34 -0800

    fixed tab to space, was very much needed. plus nagged to get changed

--- linux/trac-usermanage.sh
+++ linux/trac-usermanage.sh
@@ -3,12 +3,12 @@
 # bash insanity
 
 if [ -r ~/.trac-usermanage ]; then
-	. ~/.trac-usermanage
+ . ~/.trac-usermanage
 elif [ -r /etc/trac-usermanage.conf ]; then
-	. /etc/trac-usermanage.conf
+ . /etc/trac-usermanage.conf
 else
-	filename=$(basename $0)
-	echo "Please run $filename -s to create configuration"
+ filename=$(basename $0)
+ echo "Please run $filename -s to create configuration"
 fi
 
 
@@ -20,47 +20,47 @@ function check_root(){
 }
 
 function print_trac_users(){
-	echo "Trac Users:"
-	for user in `cat $trac_path/conf/trac.htdigest|sed -e "s/:.*//g"`
-	do
-		echo -e "\t $user"
-	done
+ echo "Trac Users:"
+ for user in `cat $trac_path/conf/trac.htdigest|sed -e "s/:.*//g"`
+ do
+  echo -e "\t $user"
+ done
 }
 
 function manage_user(){
-	htdigest "$trac_path/conf/trac.htdigest" "$realm" $1
-	trac-admin $trac_path permission add $1 $2
+ htdigest "$trac_path/conf/trac.htdigest" "$realm" $1
+ trac-admin $trac_path permission add $1 $2
 }
 
 function main(){
-	if [[ $1 == "-h" || -z $1 ]]; then
-		filename=$(basename $0)
-		echo -e "$filename -lihs [user] [capability]"
-		echo -e "\t\t -h Displays this command"
-		echo -e "\t\t -i Interactive Run"
-		echo -e "\t\t -s Create configuration file"
-		echo -e "\t\t -l List current users!"
-	elif [ $1 == "-i" ]; then
-		echo "This method has not been implimented yet"
-	elif [ $1 == "-s" ]; then
-		echo "Interactive Configuration Creation"
-		echo "Trac Project Path:"
-		read trac_path
-		echo "Trac Realm:"
-		read trac_realm
+ if [[ $1 == "-h" || -z $1 ]]; then
+  filename=$(basename $0)
+  echo -e "$filename -lihs [user] [capability]"
+  echo -e "\t\t -h Displays this command"
+  echo -e "\t\t -i Interactive Run"
+  echo -e "\t\t -s Create configuration file"
+  echo -e "\t\t -l List current users!"
+ elif [ $1 == "-i" ]; then
+  echo "This method has not been implimented yet"
+ elif [ $1 == "-s" ]; then
+  echo "Interactive Configuration Creation"
+  echo "Trac Project Path:"
+  read trac_path
+  echo "Trac Realm:"
+  read trac_realm
 
-		echo -e "trac_path=\"$trac_path\"\nrealm=\"$trac_realm\"" \
-		> /etc/trac-usermanage.conf
+  echo -e "trac_path=\"$trac_path\"\nrealm=\"$trac_realm\"" \
+  > /etc/trac-usermanage.conf
 
-	elif [ $1 == "-l" ]; then
-		print_trac_users
-	else
-		if [ -n $2 ]; then
-			manage_user $1 $2
-		else
-			echo "Incorrect Arguments"
-		fi
-	fi
+ elif [ $1 == "-l" ]; then
+  print_trac_users
+ else
+  if [ -n $2 ]; then
+   manage_user $1 $2
+  else
+   echo "Incorrect Arguments"
+  fi
+ fi
 }
 
 # Now run the actual script, like usual
